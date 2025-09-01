a89ad48bbeaba0d862e2016e09f68d2cf4aabafe
Palakfs <palakguptafs@gmail.com>
2025-09-01 22:18:12 +0530

    Add delete product functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery,useMutation, useQueryClient} from "@tanstack/react-query";
 import "./App.css";
 import {
   Table,
@@ -14,6 +14,13 @@ import { Input } from "./components/ui/input"
 import { Label } from "./components/ui/label"
 import Add_Product from "./Forms/Add_Product";
 
+function deleteProduct(id) {
+  fetch(`https://dummyjson.com/products/${id}`, {
+    method: 'DELETE',
+  }).then(res => res.json())
+    .then(console.log);
+}
+
 function App() {
   const {data} = useQuery({
     queryKey: ["product"],
@@ -24,6 +31,19 @@ function App() {
   });
 
   console.log(data);
+
+  const queryClient = useQueryClient();
+  const deleteMutation = useMutation({
+      mutationFn: deleteProduct,
+      onSuccess: () => {
+      console.log("Product deleted successfully");
+      queryClient.invalidateQueries({ queryKey: ["product"] });
+      },
+      onError: (error) => {
+      console.log("Error deleting product: ", error);
+      },
+  });
+  
 
   return (
     <div className="App">
@@ -44,6 +64,8 @@ function App() {
             <TableHead>Price</TableHead>
             <TableHead>Category</TableHead>
             <TableHead className="text-right">Stock</TableHead>
+            <TableHead></TableHead>
+            <TableHead></TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
@@ -53,6 +75,20 @@ function App() {
               <TableCell>{product.price}</TableCell>
               <TableCell>{product.category}</TableCell>
               <TableCell className="text-right">{product.stock}</TableCell>
+              <TableCell className="text-right">
+                <Button><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                </svg>
+                </Button>
+              </TableCell>
+              <TableCell>
+                <Button onClick={(e) => {
+                deleteMutation.mutate(product.id);
+              }}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                </svg>
+                </Button>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
